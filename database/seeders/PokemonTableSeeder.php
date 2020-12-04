1b51873916d4ae5fd942b979b20f7eec07dcf85a
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2020-12-04 10:29:45 -0600

    making pokemon model, migration and seeder

---
+++ database/seeders/PokemonTableSeeder.php
@@ -0,0 +1,624 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Pokemon;
+use Illuminate\Database\Seeder;
+
+class PokemonTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Pokemon::insert([
+            [
+                "name" => "caterpie",
+                "types" => "bug"
+            ],
+            [
+                "name" => "metapod",
+                "types" => "bug"
+            ],
+            [
+                "name" => "weedle",
+                "types" => "bug, poison"
+            ],
+            [
+                "name" => "kakuna",
+                "types" => "bug, poison"
+            ],
+            [
+                "name" => "ivysaur",
+                "types" => "grass, poison"
+            ],
+            [
+                "name" => "charmeleon",
+                "types" => "fire"
+            ],
+            [
+                "name" => "wartortle",
+                "types" => "water"
+            ],
+            [
+                "name" => "butterfree",
+                "types" => "bug, flying"
+            ],
+            [
+                "name" => "beedrill",
+                "types" => "bug, poison"
+            ],
+            [
+                "name" => "pidgey",
+                "types" => "normal, flying"
+            ],
+            [
+                "name" => "pidgeotto",
+                "types" => "normal, flying"
+            ],
+            [
+                "name" => "pidgeot",
+                "types" => "normal, flying"
+            ],
+            [
+                "name" => "spearow",
+                "types" => "normal, flying"
+            ],
+            [
+                "name" => "fearow",
+                "types" => "normal, flying"
+            ],
+            [
+                "name" => "pikachu",
+                "types" => "electric"
+            ],
+            [
+                "name" => "raichu",
+                "types" => "electric"
+            ],
+            [
+                "name" => "nidorina",
+                "types" => "poison"
+            ],
+            [
+                "name" => "bulbasaur",
+                "types" => "grass, poison"
+            ],
+            [
+                "name" => "venusaur",
+                "types" => "grass, poison"
+            ],
+            [
+                "name" => "charmander",
+                "types" => "fire"
+            ],
+            [
+                "name" => "charizard",
+                "types" => "fire, flying"
+            ],
+            [
+                "name" => "squirtle",
+                "types" => "water"
+            ],
+            [
+                "name" => "blastoise",
+                "types" => "water"
+            ],
+            [
+                "name" => "ninetales",
+                "types" => "fire"
+            ],
+            [
+                "name" => "golbat",
+                "types" => "poison, flying"
+            ],
+            [
+                "name" => "oddish",
+                "types" => "grass, poison"
+            ],
+            [
+                "name" => "rattata",
+                "types" => "normal"
+            ],
+            [
+                "name" => "gloom",
+                "types" => "grass, poison"
+            ],
+            [
+                "name" => "raticate",
+                "types" => "normal"
+            ],
+            [
+                "name" => "vileplume",
+                "types" => "grass, poison"
+            ],
+            [
+                "name" => "ekans",
+                "types" => "poison"
+            ],
+            [
+                "name" => "arbok",
+                "types" => "poison"
+            ],
+            [
+                "name" => "parasect",
+                "types" => "bug, grass"
+            ],
+            [
+                "name" => "sandshrew",
+                "types" => "ground"
+            ],
+            [
+                "name" => "magikarp",
+                "types" => "water"
+            ],
+            [
+                "name" => "ditto",
+                "types" => "normal"
+            ],
+            [
+                "name" => "venonat",
+                "types" => "bug, poison"
+            ],
+            [
+                "name" => "sandslash",
+                "types" => "ground"
+            ],
+            [
+                "name" => "nidorino",
+                "types" => "poison"
+            ],
+            [
+                "name" => "venomoth",
+                "types" => "bug, poison"
+            ],
+            [
+                "name" => "diglett",
+                "types" => "ground"
+            ],
+            [
+                "name" => "dugtrio",
+                "types" => "ground"
+            ],
+            [
+                "name" => "nidoran-f",
+                "types" => "poison"
+            ],
+            [
+                "name" => "nidoqueen",
+                "types" => "poison, ground"
+            ],
+            [
+                "name" => "nidoran-m",
+                "types" => "poison"
+            ],
+            [
+                "name" => "arcanine",
+                "types" => "fire"
+            ],
+            [
+                "name" => "poliwag",
+                "types" => "water"
+            ],
+            [
+                "name" => "nidoking",
+                "types" => "poison, ground"
+            ],
+            [
+                "name" => "poliwhirl",
+                "types" => "water"
+            ],
+            [
+                "name" => "poliwrath",
+                "types" => "water, fighting"
+            ],
+            [
+                "name" => "zubat",
+                "types" => "poison, flying"
+            ],
+            [
+                "name" => "abra",
+                "types" => "psychic"
+            ],
+            [
+                "name" => "clefable",
+                "types" => "fairy"
+            ],
+            [
+                "name" => "vulpix",
+                "types" => "fire"
+            ],
+            [
+                "name" => "jigglypuff",
+                "types" => "normal, fairy"
+            ],
+            [
+                "name" => "bellsprout",
+                "types" => "grass, poison"
+            ],
+            [
+                "name" => "weepinbell",
+                "types" => "grass, poison"
+            ],
+            [
+                "name" => "wigglytuff",
+                "types" => "normal, fairy"
+            ],
+            [
+                "name" => "victreebel",
+                "types" => "grass, poison"
+            ],
+            [
+                "name" => "tentacruel",
+                "types" => "water, poison"
+            ],
+            [
+                "name" => "graveler",
+                "types" => "rock, ground"
+            ],
+            [
+                "name" => "ponyta",
+                "types" => "fire"
+            ],
+            [
+                "name" => "rapidash",
+                "types" => "fire"
+            ],
+            [
+                "name" => "magnemite",
+                "types" => "electric, steel"
+            ],
+            [
+                "name" => "magneton",
+                "types" => "electric, steel"
+            ],
+            [
+                "name" => "doduo",
+                "types" => "normal, flying"
+            ],
+            [
+                "name" => "dodrio",
+                "types" => "normal, flying"
+            ],
+            [
+                "name" => "seel",
+                "types" => "water"
+            ],
+            [
+                "name" => "dewgong",
+                "types" => "water, ice"
+            ],
+            [
+                "name" => "shellder",
+                "types" => "water"
+            ],
+            [
+                "name" => "cloyster",
+                "types" => "water, ice"
+            ],
+            [
+                "name" => "kingler",
+                "types" => "water"
+            ],
+            [
+                "name" => "voltorb",
+                "types" => "electric"
+            ],
+            [
+                "name" => "electrode",
+                "types" => "electric"
+            ],
+            [
+                "name" => "exeggutor",
+                "types" => "grass, psychic"
+            ],
+            [
+                "name" => "paras",
+                "types" => "bug, grass"
+            ],
+            [
+                "name" => "hitmonlee",
+                "types" => "fighting"
+            ],
+            [
+                "name" => "hitmonchan",
+                "types" => "fighting"
+            ],
+            [
+                "name" => "weezing",
+                "types" => "poison"
+            ],
+            [
+                "name" => "horsea",
+                "types" => "water"
+            ],
+            [
+                "name" => "seadra",
+                "types" => "water"
+            ],
+            [
+                "name" => "goldeen",
+                "types" => "water"
+            ],
+            [
+                "name" => "seaking",
+                "types" => "water"
+            ],
+            [
+                "name" => "staryu",
+                "types" => "water"
+            ],
+            [
+                "name" => "starmie",
+                "types" => "water, psychic"
+            ],
+            [
+                "name" => "electabuzz",
+                "types" => "electric"
+            ],
+            [
+                "name" => "magmar",
+                "types" => "fire"
+            ],
+            [
+                "name" => "eevee",
+                "types" => "normal"
+            ],
+            [
+                "name" => "vaporeon",
+                "types" => "water"
+            ],
+            [
+                "name" => "jolteon",
+                "types" => "electric"
+            ],
+            [
+                "name" => "flareon",
+                "types" => "fire"
+            ],
+            [
+                "name" => "porygon",
+                "types" => "normal"
+            ],
+            [
+                "name" => "omastar",
+                "types" => "rock, water"
+            ],
+            [
+                "name" => "kabuto",
+                "types" => "rock, water"
+            ],
+            [
+                "name" => "articuno",
+                "types" => "ice, flying"
+            ],
+            [
+                "name" => "zapdos",
+                "types" => "electric, flying"
+            ],
+            [
+                "name" => "moltres",
+                "types" => "fire, flying"
+            ],
+            [
+                "name" => "dragonair",
+                "types" => "dragon"
+            ],
+            [
+                "name" => "clefairy",
+                "types" => "fairy"
+            ],
+            [
+                "name" => "meowth",
+                "types" => "normal"
+            ],
+            [
+                "name" => "persian",
+                "types" => "normal"
+            ],
+            [
+                "name" => "psyduck",
+                "types" => "water"
+            ],
+            [
+                "name" => "golduck",
+                "types" => "water"
+            ],
+            [
+                "name" => "mankey",
+                "types" => "fighting"
+            ],
+            [
+                "name" => "primeape",
+                "types" => "fighting"
+            ],
+            [
+                "name" => "growlithe",
+                "types" => "fire"
+            ],
+            [
+                "name" => "kadabra",
+                "types" => "psychic"
+            ],
+            [
+                "name" => "alakazam",
+                "types" => "psychic"
+            ],
+            [
+                "name" => "machop",
+                "types" => "fighting"
+            ],
+            [
+                "name" => "machoke",
+                "types" => "fighting"
+            ],
+            [
+                "name" => "machamp",
+                "types" => "fighting"
+            ],
+            [
+                "name" => "tentacool",
+                "types" => "water, poison"
+            ],
+            [
+                "name" => "geodude",
+                "types" => "rock, ground"
+            ],
+            [
+                "name" => "golem",
+                "types" => "rock, ground"
+            ],
+            [
+                "name" => "slowpoke",
+                "types" => "water, psychic"
+            ],
+            [
+                "name" => "slowbro",
+                "types" => "water, psychic"
+            ],
+            [
+                "name" => "farfetchd",
+                "types" => "normal, flying"
+            ],
+            [
+                "name" => "grimer",
+                "types" => "poison"
+            ],
+            [
+                "name" => "muk",
+                "types" => "poison"
+            ],
+            [
+                "name" => "gastly",
+                "types" => "ghost, poison"
+            ],
+            [
+                "name" => "haunter",
+                "types" => "ghost, poison"
+            ],
+            [
+                "name" => "gengar",
+                "types" => "ghost, poison"
+            ],
+            [
+                "name" => "onix",
+                "types" => "rock, ground"
+            ],
+            [
+                "name" => "drowzee",
+                "types" => "psychic"
+            ],
+            [
+                "name" => "hypno",
+                "types" => "psychic"
+            ],
+            [
+                "name" => "krabby",
+                "types" => "water"
+            ],
+            [
+                "name" => "exeggcute",
+                "types" => "grass, psychic"
+            ],
+            [
+                "name" => "cubone",
+                "types" => "ground"
+            ],
+            [
+                "name" => "marowak",
+                "types" => "ground"
+            ],
+            [
+                "name" => "lickitung",
+                "types" => "normal"
+            ],
+            [
+                "name" => "koffing",
+                "types" => "poison"
+            ],
+            [
+                "name" => "rhyhorn",
+                "types" => "ground, rock"
+            ],
+            [
+                "name" => "rhydon",
+                "types" => "ground, rock"
+            ],
+            [
+                "name" => "chansey",
+                "types" => "normal"
+            ],
+            [
+                "name" => "tangela",
+                "types" => "grass"
+            ],
+            [
+                "name" => "kangaskhan",
+                "types" => "normal"
+            ],
+            [
+                "name" => "mr-mime",
+                "types" => "psychic, fairy"
+            ],
+            [
+                "name" => "scyther",
+                "types" => "bug, flying"
+            ],
+            [
+                "name" => "jynx",
+                "types" => "ice, psychic"
+            ],
+            [
+                "name" => "pinsir",
+                "types" => "bug"
+            ],
+            [
+                "name" => "tauros",
+                "types" => "normal"
+            ],
+            [
+                "name" => "gyarados",
+                "types" => "water, flying"
+            ],
+            [
+                "name" => "lapras",
+                "types" => "water, ice"
+            ],
+            [
+                "name" => "omanyte",
+                "types" => "rock, water"
+            ],
+            [
+                "name" => "kabutops",
+                "types" => "rock, water"
+            ],
+            [
+                "name" => "aerodactyl",
+                "types" => "rock, flying"
+            ],
+            [
+                "name" => "snorlax",
+                "types" => "normal"
+            ],
+            [
+                "name" => "dratini",
+                "types" => "dragon"
+            ],
+            [
+                "name" => "dragonite",
+                "types" => "dragon, flying"
+            ],
+            [
+                "name" => "mewtwo",
+                "types" => "psychic"
+            ],
+            [
+                "name" => "mew",
+                "types" => "psychic"
+            ]
+        ]);
+    }
+}
