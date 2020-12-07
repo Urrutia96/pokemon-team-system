0288cab4b393fa85021fd5c01268cb8da7f3f52c
Santos Osmin Urrutia <urrutia.osmin@gmail.com>
2020-12-06 22:53:48 -0600

    adding pokemon team api routes

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\API\PokemonController;
 use App\Http\Controllers\API\RegisterController;
 use App\Http\Controllers\API\TeamController;
+use App\Http\Controllers\API\PokemonTeamController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('register', RegisterController::class)->only(['store']);
 Route::apiResource('pokemon', PokemonController::class);
 Route::apiResource('teams', TeamController::class)->only('index');
+Route::apiResource('teams.pokemon', PokemonTeamController::class);
